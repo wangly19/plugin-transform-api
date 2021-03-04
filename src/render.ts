5bbbf602a3470114f79442fee35ca833b3ad50b7
wangly19 <1418202350@qq.com>
2021-03-05 01:03:03 +0800

    chore(render): 修改部分函数类型

--- src/render.ts
+++ src/render.ts
@@ -1,65 +1,89 @@
-import { type } from 'os'
-import { 
-  RequestBaseConfig,
-  RequestMethod,
-  RequestMethodAndURL,
-} from './util'
+import { RequestBaseConfig, RequestMethod, RequestMethodAndURL } from './util';
+
+import { utils } from 'umi';
 
 export type RequestASTModule = {
-  functionName: string,
-  url: string
-  method: RequestMethod
-  params?: Record<string, keyof any>
-  data?: Record<string, keyof any>
-  query?: Record<string, keyof any>
-}
+  functionName: string;
+  url: string;
+  method: RequestMethod;
+  linkParams?: Array<string>;
+};
 
-export type TupleAfterCutting = [RequestMethod, string]
+export type TupleAfterCutting = [RequestMethod, string];
 
 /**
  * 转换请求函数为AST抽象模块
- * @returns { RequestASTModule }
+ * @param { RequestBaseConfig } exportServiceModule 导出的配置对象
+ * @returns { Array<RequestASTModule> }
  */
-export function transformRequestFunctionModule (
-  exportServiceMoudle: RequestBaseConfig
-): RequestASTModule {
-
+export function transformRequestFunctionModule(
+  exportServiceModule: RequestBaseConfig,
+): Array<RequestASTModule> {
   // 遍历对象
-  const requestParameter: Array<RequestASTModule> =  Object.keys(
-    exportServiceMoudle
-  ).map(((k: string) => {
-    const value: RequestMethodAndURL = exportServiceMoudle[k]
-    const [ method, transformPreUrl ]: TupleAfterCutting = getCurrentMethodAndURL(value)
+  const requestParameter: Array<RequestASTModule> = Object.keys(
+    exportServiceModule,
+  ).map((k: string) => {
+    const value: RequestMethodAndURL = exportServiceModule[k];
+    const [method, transformPreUrl]: TupleAfterCutting = getCurrentMethodAndURL(
+      value,
+    );
 
     // throw error
     if (method) {
-      throw new Error(`[error]: 没有找到当前配置的请求方法，请检查${k}的配置是否正确。`)
+      throw new Error(
+        `[error]: 没有找到当前配置的请求方法，请检查${k}的配置是否正确。`,
+      );
     }
 
     // throw error
     if (transformPreUrl) {
-      throw new Error(`[error]: 没有找到当前配置的请求链接api，请检查${k}的配置是否正确。`)
+      throw new Error(
+        `[error]: 没有找到当前配置的请求链接api，请检查${k}的配置是否正确。`,
+      );
     }
 
-    const { transformAfterURL, linkParameter } = getCurrentLinkParamsAndURL(transformPreUrl)
+    const { transformAfterURL, linkParameter } = getCurrentLinkParamsAndURL(
+      transformPreUrl,
+    );
 
-    // TODO
     return {
       functionName: k,
-    }
-  }))
+      url: transformAfterURL,
+      method,
+      linkParams: linkParameter,
+    };
+  });
+
+  return requestParameter;
 }
 
+/**
+ * 获取所有转换后的AST语法模块
+ * @returns { Array<RequestASTModule> }
+ */
+export function getAllRequestModule(
+  requestConfig: Array<RequestBaseConfig>,
+): Array<RequestASTModule> {
+  const currentRequestConfigModule: RequestASTModule[][] = requestConfig.map(
+    (config: RequestBaseConfig): Array<RequestASTModule> => {
+      return transformRequestFunctionModule(config);
+    },
+  );
+
+  return utils.lodash.flatten(currentRequestConfigModule);
+}
+
+export function generateRequestFunctionModules() {}
 
 /**
  * 切割当前URL和Method为元组方法
  * @param { RequestMethodAndURL } configAttr 当前配置的url和method
  * @returns { TupleAfterCutting }
  */
-export function getCurrentMethodAndURL (
-  configAttr: RequestMethodAndURL
+export function getCurrentMethodAndURL(
+  configAttr: RequestMethodAndURL,
 ): TupleAfterCutting {
-  return configAttr.split(' ') as TupleAfterCutting
+  return configAttr.split(' ') as TupleAfterCutting;
 }
 
 /**
@@ -67,21 +91,15 @@ export function getCurrentMethodAndURL (
  * @param { string } transformBeforeURL 转换前url
  * @returns { object }
  */
-export function getCurrentLinkParamsAndURL (transformBeforeURL: string): {
-  transformAfterURL: string,
-  linkParameter: Array<string>,
+export function getCurrentLinkParamsAndURL(
+  transformBeforeURL: string,
+): {
+  transformAfterURL: string;
+  linkParameter: Array<string>;
 } {
-  const [transformAfterURL, ...linkParameter] = transformBeforeURL.split('/:')
+  const [transformAfterURL, ...linkParameter] = transformBeforeURL.split('/:');
   return {
     transformAfterURL,
-    linkParameter
-  }
+    linkParameter,
+  };
 }
-
-/**
- * 获取所有转换后的AST语法模块
- * @returns { Array<RequestASTModule> }
- */
-export function getAllRequestModule () {}
-
-export function generateRequestFunctionModules () {}
